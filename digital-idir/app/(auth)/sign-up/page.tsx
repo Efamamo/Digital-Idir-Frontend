0cb6dcb9cbe26d3b8062c1d91f038502112a03e0
ephrem <ephremmamo555@gmail.com>
2024-10-21 09:59:44 +0300

    initialize the project

---
+++ digital-idir/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SignUp() {
+  return (
+    <section>
+      <h1 className="text-3xl font-bold">Sign Up</h1>
+    </section>
+  );
+}
+
+export default SignUp;
